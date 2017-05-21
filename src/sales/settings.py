03b435ba3e200fa55453e7dd73d42e8467ba40e8
@slovacus <slovacus@gmail.com>
2017-05-21 17:37:56 -0500

    chore: implement paginator (#2)

--- src/sales/settings.py
+++ src/sales/settings.py
@@ -107,6 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
