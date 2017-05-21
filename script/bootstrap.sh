bb503e804f19248e9f008f833988e1aff373bc26
@slovacus <slovacus@gmail.com>
2017-05-21 15:27:45 -0500

    chore: implement script for python (#1)

---
+++ script/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+export PROJECT_NAME=python-example-drf
+
+export PYTHON_VERSION=3.6.0
+export PYENV_NAME="${PROJECT_NAME}"
+
+export GRIP_PORT=6430
+
+# Vars Dir
+export ROOT_DIR
+ROOT_DIR=$(pwd)
+export RESOURCES_DIR="$ROOT_DIR/resources"
+export RESOURCES_DB_DIR="$RESOURCES_DIR/db"
+export PROVISION_DIR="$ROOT_DIR/provision/ansible"
+export SOURCE_DIR="${ROOT_DIR}/"
+export REQUIREMENTS_DIR="${SOURCE_DIR}/requirements/"
