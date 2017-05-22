13fa69823ae1ba20a6e8c189852a9d68d46ef046
@slovacus <slovacus@gmail.com>
2017-05-22 11:32:52 -0500

    !#2 Fix: data response

--- src/sales/pagination.py
+++ src/sales/pagination.py
@@ -1,24 +1,8 @@
 from rest_framework import pagination
 from rest_framework.response import Response
-from rest_framework.utils.urls import replace_query_param
 
 
 class CustomCoursePaginator(pagination.PageNumberPagination):
 
     def get_paginated_response(self, data):
-        return Response({'count': self.page.paginator.count,
-                         'next': self.get_next_link(),
-                         'previous': self.get_previous_link(),
-                         'result': data})
-
-    def get_next_link(self):
-        if not self.page.has_next():
-            return None
-        page_number = self.page.next_page_number()
-        return replace_query_param('', self.page_query_param, page_number)
-
-    def get_previous_link(self):
-        if not self.page.has_previous():
-            return None
-        page_number = self.page.previous_page_number()
-        return replace_query_param('', self.page_query_param, page_number)
+        return Response({'result': data})
